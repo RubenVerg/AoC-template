939e5f0374e304f1fcc1a813f6583347ccb62b6d
Ruben Vergani <RubenVerg@outlook.com>
2020-11-25 10:13:46 +0100

    adds support for automatically fetching input

--- src/tools/main.ts
+++ src/tools/main.ts
@@ -1,9 +1,9 @@
 import read from './reader';
 import { format } from 'util';
-import { DayResolver } from '../types/types';
+import type { DayResolver, Day } from '../types/types';
 
 export default function easier<T>(f: (input: string) => Promise<T>) {
-	return async function (d: number) {
+	return async function (d: Day) {
 		return format(await f(await read(d)))
 	} as DayResolver;
 }
