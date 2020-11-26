3d31e56e1f96265d6236b5cc72744314a007b5c2
Ruben Vergani <RubenVerg@outlook.com>
2020-11-26 10:06:02 +0100

    adds the common bug list

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,10 @@ async function main() {
 
 		Options
 		--year, -y: Do not infer the latest year, but specify one.
+		--resolve: Run the error resolution tool.
 
 		Examples
-		$ ts-node . 1a 1b 2a 2b
+		$ ts-node src/main 1a 1b 2a 2b
 		1a:\tnull
 		1b:\tnull
 		2a:\tnull
@@ -23,10 +24,14 @@ async function main() {
 
 		# or
 
-		$ ts-node --year 2015 1a
+		$ ts-node src/main --year 2015 1a
 		1a:\tnull
 		$ cat data/1
 		<input for 2015 day 1>
+
+		# or
+
+		$ ts-node src/main --resolve
 
 		NB
 		Edit src/tools/token to export your Session cookie.
@@ -36,15 +41,23 @@ async function main() {
 				year: {
 					alias: 'y',
 					type: 'number'
+				},
+				resolve: {
+					type: 'boolean',
+					default: false
 				}
 			}
 		}
 	)
 
-	for (let day of input.map(i => i.trim().toLowerCase())) {
-		const f = (await import(`./${day}`)).default as types.DayResolver;
+	if (flags.resolve) {
+		console.log(`Common errors:\n\tInternal Server Error: either your token is wrong or you are trying to get an unexisting year.\n\tCannot find module *: there is no script for the day you are trying to run.\n\tFound an error not in the list? Either check your code or report an issue!`);
+	} else {
+		for (let day of input.map(i => i.trim().toLowerCase())) {
+			const f = (await import(`./${day}`)).default as types.DayResolver;
 
-		console.log(`${day}\t: ${await f(parseInt(day) as types.Day, flags.year as types.Year)}`);
+			console.log(`${day}\t: ${await f(parseInt(day) as types.Day, flags.year as types.Year)}`);
+		}
 	}
 }
 
